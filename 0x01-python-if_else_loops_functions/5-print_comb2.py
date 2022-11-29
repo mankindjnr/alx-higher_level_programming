0189ff6c0e51fe40fd8052662198c60e3cae9c60
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-11-29 10:09:58 -0800

    numbers comma space

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print(str(0)+str(i), end=', ')
+    if i != 99:
+        print("{:02d}".format(i), end=', ')
     else:
-        print(i, end=', ')
+        print("{:d}".format(i))
