2be3dbbf6f3a86a5838987bb82da3ba277294301
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-11-30 04:47:12 -0800

    repair

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    length = len(str)
+    for i in range(0, length):
+        if str[i] >= 'a' and str[i] <= 'z':
+            print("{:c}".format(ord(str[i]) - 32), end='')
+        else:
+            print(str[i], end='')
