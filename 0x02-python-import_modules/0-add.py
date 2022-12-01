2a1ac1af37386cd3ead2dbaeb8d908fe4bf387b2
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-12-01 03:14:32 -0800

    importing a function from a file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import add_0 as sum
+from add_0 import add
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, sum.add(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
