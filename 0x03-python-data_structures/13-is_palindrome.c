fca607f040f8c4d9dc6e43f45df38a6138903668
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-12-05 07:56:49 -0800

    checking if a singly linked list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ *is_palindrome - checking if a singly linked list is a palindrome
+ *@head: the pointer to the starting node
+ *Return: 0-not palindrome 1-if palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	int arraySize = 0, i, j, count = 0;
+
+	while ((*head) != NULL)
+	{
+		arraySize++;
+		*head = (*head)->next;
+	}
+
+	int first[arraySize];
+	int second[arraySize];
+
+	if ((*head) == NULL)
+	{
+		return (1);
+	}
+
+	while ((*head) != NULL)
+	{
+		first[count] = (*head)->n;
+		*head = (*head)->next;
+		count++;
+	}
+
+	for (i = 0; i < arraySize; i++)
+	{
+		second[0] = first[(arraySize - 1) - i];
+	}
+
+	for (j = 0; j < arraySize; j++)
+	{
+		if (second[j] != first[j])
+		{
+			return (0);
+		}
+	}
+
+	return (1);
+}
---
+++ 0x03-python-data_structures/arr.c
@@ -0,0 +1,10 @@
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+	char *arr;
+
+	arr = "1";
+	printf("first: %c\n", arr[0]);
+}
