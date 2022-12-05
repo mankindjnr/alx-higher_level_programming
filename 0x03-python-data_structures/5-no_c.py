f72a65ca99335751aa18c76f7df0db035d6fa51f
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-12-05 01:17:14 -0800

    remove character c and C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,8 @@ def no_c(my_string):
     new_string = ""
     for i in range(len(my_string)):
         if my_string[i] != 'c' and my_string[i] != 'C':
-            new_string += my_string[i]
+            new_string = new_string + my_string[i]
+        else:
+            continue
 
     return new_string
