08964d612344a566e8ae34cd11550c77ec0c0abb
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-12-05 01:41:25 -0800

    reverse a linked list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
-    reversed = my_list[::-1]
+    reversed = list()
+    for item in my_list:
+        reversed = [item] + reversed
 
-    if my_list:
-        for i in range(length):
-            print("{:d}".format(reversed[i]))
+    for i in range(length):
+        print("{:d}".format(reversed[i]))
