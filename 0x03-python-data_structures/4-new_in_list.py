bcf62dff30db68c85e1dc9f47d456bb73e2e162d
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-12-05 01:28:16 -0800

    appending and change a list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,12 @@
 def new_in_list(my_list, idx, element):
     length = len(my_list)
     new_list = []
-    new_list = new_list + my_list
     if idx < 0 or idx > length:
         return my_list
 
-    new_list[idx] = element
+    for i in range(length):
+        if i != idx:
+            new_list.append(my_list[i])
+        else:
+            new_list.append(element)
     return new_list
