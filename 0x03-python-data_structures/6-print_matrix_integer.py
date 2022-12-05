f7dd3ecd89ced5cb3da8f8821f8821fba40d0525
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-12-05 01:49:02 -0800

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,3 @@ def print_matrix_integer(matrix=[[]]):
     output = '\n'.join([''.join(['{:2}'.format(item) for item in row]) for row
                         in matrix])
     print(output)
-    return output
