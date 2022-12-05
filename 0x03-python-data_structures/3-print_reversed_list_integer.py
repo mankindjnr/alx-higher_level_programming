b615008a7669fff099d60618b657347bbb909db7
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-12-05 01:42:58 -0800

    reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
     reversed = list()
+    if length == 0:
+        return
     for item in my_list:
         reversed = [item] + reversed
 
