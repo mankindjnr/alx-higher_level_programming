183a103d43ac67277ba5d532669cdcc0fa51c4e6
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-12-09 07:58:07 -0800

    simple dictionary key delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
+    new_dict = a_dictionary.copy()
+    if key in new_dict:
+        del new_dict[key]
 
-    return a_dictionary
+    return new_dict
