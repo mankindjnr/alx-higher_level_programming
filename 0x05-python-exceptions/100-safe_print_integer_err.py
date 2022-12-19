9eb8560ac468cb03d206ccba089b5b8f1b58a2eb
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-12-19 07:35:33 -0800

    saving traceback, print safely

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+from sys import *
 def safe_print_integer_err(value):
-    import sys
     try:
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
-        print("Exception: {}".format(exc_info()[1]), file=sys.stderr)
+        print("Exception: {}".format(exc_info()[1]), file=stderr)
         return False
