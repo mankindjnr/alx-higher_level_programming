42c13058712184b01720fa7f65eec7c30e359a9b
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-12-20 06:08:54 -0800

    define a square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("1-square").__define a square__)'"""
+
+
+class Square:
+    """python3 -c 'print(__import__("1-Square").Square.__square__)'"""
+    def __init__(self, size):
+        self.__size = None
