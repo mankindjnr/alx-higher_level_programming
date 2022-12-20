74022c83f1fbb302b3bfe131e5a561eb8ce35d4f
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-12-20 04:02:47 -0800

    empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,5 +3,7 @@
 """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
 """'print(__import__("my_module").my_function.__doc__)'"""
 """python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'"""
+
+
 class Square:
     pass
