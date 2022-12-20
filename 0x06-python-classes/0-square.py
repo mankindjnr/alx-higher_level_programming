f70e9270c595bc957f168217c291bb1e77b46ccb
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-12-20 04:01:27 -0800

    empty class to define Square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+"""python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+"""'print(__import__("my_module").my_function.__doc__)'"""
+"""python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'"""
 class Square:
     pass
