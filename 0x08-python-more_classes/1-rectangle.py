e961f8e6c94fb9f6a36446e9ce088f4d5f0ede7b
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-01-05 01:13:08 -0800

    define a rectangle with getters and setters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,31 +6,30 @@ class Rectangle:
     """the rectangle class defined with width and height"""
     # private instance attribute width and height
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
-    # width property getter
+    # property getter
     def width(self):
         return self.__width
 
-    # width property setter
-    def width(self, value):
-        if isinstance(value, int) and value >= 0:
-            self.__width = value
-        elif value < 0:
-            raise ValueError("width must be >- 0")
-        else:
-            raise TypeError("width must be an integer")
-
-    # getter height
     def height(self):
         return self.__height
 
+    # property setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
     # height setter
     def height(self, ValueHeight):
-        if isinstance(ValueHeight, int) and ValueHeight >= 0:
-            self.__height = ValueHeight
-        elif ValueHeight < 0:
-            raise ValueError("height must be >= 0")
-        else:
+        if not isinstance(ValueHeight, int):
             raise TypeError("height must be an integer")
+        if ValueHeight < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = ValueHeight
