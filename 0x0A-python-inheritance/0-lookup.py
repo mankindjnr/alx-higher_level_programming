5ce07b09d5fb82353b682fd80c9f0dcd73f28706
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-01-09 03:16:13 -0800

    return a list of atrib abd methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a function that returns alist of available attr' and methods of an obj"""
+
+
+def lookup(obj):
+    """you can use the dir function to get a list of attr and methods"""
+    return dir(obj)
