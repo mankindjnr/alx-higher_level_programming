fc3f3c2b6e91cf0d57531e5c88d55765df8a1e51
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-01-09 05:53:37 -0800

    inherits from a subclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A function that returns true if the object is an instance of a subclass
+that inherits directly from a specific class
+"""
+
+
+def inherits_from(obj, a_class):
+    """checking if its an instance of a subclass inheriting a specific cls"""
+    return isinstance(obj, a_class)
