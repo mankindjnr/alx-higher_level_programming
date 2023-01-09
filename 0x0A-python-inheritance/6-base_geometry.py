044c5b3343be2f5ec4c6627467b474b70518c052
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-01-09 06:12:50 -0800

    raising an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""improving the empty class geometry"""
+
+
+class BaseGeometry:
+    """raising an exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
