b7b3dfef75025489886053ecbd9a2e8639933abc
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-01-10 03:31:15 -0800

    reading a file txt

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,11 @@
 
 
 def read_file(filename=""):
-    """the function reading a text file"""
-    with open(filename, encoding="utf-8")as read_file:
+    """
+    the function reading a text file
+    with open(filename, mode="r", encoding="utf-8")as read_file:
         print(read_file.read())
+    """
+    with open(filename)as read_file:
+        for line in read_file:
+            print(line, end='')
