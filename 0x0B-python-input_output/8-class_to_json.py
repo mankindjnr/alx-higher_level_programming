f81a7d2ca57425d7d004999eca2c0a3df53ef23d
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-01-10 07:34:31 -0800

    Convert an object's attributes into a dictionary for JSON serialization

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """function that JSON serializes an object"""
-import json
 
 
 def class_to_json(obj):
-    """JSON serializing by defininta default method to return the dict"""
-    def default(o):
-        """
-        defining a default method that returns a dictionary rep of that
-        object and the json.dumps to serialize the object with the default
-        parameter set to the default method defined above
-        """
-        return o.__dict__
-    return json.dumps(obj, default=default)
+    """converting an object attri to a dictionary for JSON serialization"""
+    obj_dict = {}
+    for key in obj.__dict__:
+        obj_dict[key] = obj.__dict__[key]
+
+    return obj_dict
