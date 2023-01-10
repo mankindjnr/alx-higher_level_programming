1a2ea66fbd68aca82223164d941f18948af45a38
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-01-10 04:33:23 -0800

    JSON representation of an object string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Json representation of an object"""
+import json
+
+
+def to_json_string(my_obj):
+    """a function that returns the JSON rep of an object string"""
+    return json.dumps(my_obj)
