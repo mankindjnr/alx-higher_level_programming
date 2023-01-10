a0a57421257b3f39150a330246dfa278bbe1e871
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-01-10 07:25:19 -0800

    class to json serializing

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,7 +4,12 @@ import json
 
 
 def class_to_json(obj):
-    """JSON serializing"""
+    """JSON serializing by defininta default method to return the dict"""
     def default(o):
+        """
+        defining a default method that returns a dictionary rep of that
+        object and the json.dumps to serialize the object with the default
+        parameter set to the default method defined above
+        """
         return o.__dict__
     return json.dumps(obj, default=default)
