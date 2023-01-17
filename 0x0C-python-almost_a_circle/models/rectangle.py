c781c831a0f2edfa1064e8dbb82645f140d404ea
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-01-17 02:01:20 -0800

    rectangle args method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,6 +95,21 @@ class Rectangle(Base):
             rectangle += (" " * self.x) + ("#" * self.width) + "\n"
         print(rectangle, end="")
 
+    def update(self, *args):
+        '''
+        a public method that assigns an argument to each attribute
+        '''
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+
     @staticmethod
     def setter_validation(attribute, value):
         if type(value) != int:
