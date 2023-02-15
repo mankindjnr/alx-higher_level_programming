89bf6a5e6da9c32ee1824787881d3d8970dba134
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-02-15 05:20:15 -0800

    fix amazon download

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,4 @@
--- lists all genres from hbtn_0d_tvshows and displays the number of shows
--- linked to each.
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked
 SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_shows
 FROM tv_genres
 JOIN tv_show_genres
