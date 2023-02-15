ff42857bbf3cda8412c59325d81cbf6c7b3cf257
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-02-15 05:09:56 -0800

    before dexter

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,5 @@
--- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows
+-- linked to each.
 SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_shows
 FROM tv_genres
 JOIN tv_show_genres
