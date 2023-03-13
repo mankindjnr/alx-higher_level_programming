9dde1eef4e579da8828340af88a7e646f545f7c3
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-03-13 07:52:53 -0700

    removed

--- 0x12-javascript-warm_up/2-arguments.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-if (process.argv.length === 2) {
-  console.log('No argumnet');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
