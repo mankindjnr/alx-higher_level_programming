7460ebdc29fad7db400c88a3c4448eea86b139fd
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-03-13 04:34:46 -0700

    convert to integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const firstArg = process.argv[2];
+let convertFirstArg;
+
+if (!(+firstArg)) {
+  console.log('Not a number');
+} else {
+  convertFirstArg = parseInt(firstArg);
+  console.log('My number: ', convertFirstArg);
+}
