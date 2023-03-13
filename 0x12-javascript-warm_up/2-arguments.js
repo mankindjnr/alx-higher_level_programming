f5d88652e609600da926f54586318a9010d84e08
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-03-13 03:39:08 -0700

    parsed argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 if (process.argv.length === 2) {
   console.log('No argumnet');
-} else
-if (process.argv.length === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
