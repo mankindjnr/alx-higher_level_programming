2d86e15757518e6b5df7ba457a11d1d4bfc9e475
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-03-31 03:51:59 -0700

    display value id in header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,5 +9,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as resp:
-        X_Request_Id = response.headers.get('X-Request-Id')
+        X_Request_Id = resp.headers.get('X-Request-Id')
         print(X_Request_Id)
