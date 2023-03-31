6204dac7b646027935d3476c2d249b89258c9368
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-03-31 03:49:34 -0700

    display value id in header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL, sends a request to the URL and
+displays the value of the X-Request-Id variable in the header of the resp.
+"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as resp:
+        X_Request_Id = response.headers.get('X-Request-Id')
+        print(X_Request_Id)
