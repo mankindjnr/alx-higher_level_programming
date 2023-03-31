44b2ed7a1d0db38942c9a2b1f8fcea645c2e7f40
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-03-31 07:40:26 -0700

    header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,7 +9,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-
-if 'X-Request-Id' in response.headers:
     request_id = response.headers['X-Request-Id']
     print(request_id)
