bad2881dee1ecbf9d0c3aefdd3f1d8a7033458e2
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-03-31 09:05:19 -0700

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,8 +11,7 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    code_status = response.ok
-    if code_status is True:
+    if response.status_code < 400:
         print(response.text)
     else:
         print("Error code {}".format(response.status_code))
