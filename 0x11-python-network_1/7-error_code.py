fcfefbe5d278c43f0e71cf661588a63f35a0a51c
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-03-31 09:11:19 -0700

    error code status

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """
-Write a Python script that takes in a URL,
-sends a request to the URL and displays the body of the response.
-If the HTTP status code is greater than or equal to 400, print:
-Error code: followed by the value of the HTTP status
+outputing error codes and detecting them
 """
 import sys
 import requests
@@ -11,7 +8,8 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    if response.status_code < 400:
+    code_status = response.ok
+    if code_status is True:
         print(response.text)
     else:
-        print("Error code {}".format(response.status_code))
+        print("Error code: {}".format(response.status_code))
